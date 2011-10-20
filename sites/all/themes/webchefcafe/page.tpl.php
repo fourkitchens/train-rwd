40e34a62890b2633588ebee196914efaa853541f
Todd Nienkerk <todd@fourkitchens.com>
2011-10-20 12:23:33 -0500

    Changes to webchef theme.

--- sites/all/themes/webchefcafe/page.tpl.php
+++ sites/all/themes/webchefcafe/page.tpl.php
@@ -102,20 +102,14 @@
           <li><a href="#menu">Menu</a></li>
           <li><a href="#about">About</a></li>
         </ul>
-        <?php /* print theme('links__system_main_menu', array('links' => $cafe_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); */ ?>
       </div></div> <!-- /.section, /#navigation -->
     <?php endif; ?>
-
-    <?php /* if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; */ ?>
 
     <?php print $messages; ?>
 
     <div id="main-wrapper"><div id="main" class="clearfix">
 
       <div id="content" class="column"><div class="section">
-        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
@@ -127,18 +121,17 @@
         <?php print $feed_icons; ?>
       </div></div> <!-- /.section, /#content -->
 
-      <?php if ($page['sidebar_first']): ?>
-        <div id="sidebar-first" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_first']); ?>
-        </div></div> <!-- /.section, /#sidebar-first -->
+      <?php if ($page['sidebar']): ?>
+        <div id="sidebar" class="column sidebar"><div class="section">
+          <?php print render($page['sidebar']); ?>
+        </div></div> <!-- /.section, /#sidebar -->
       <?php endif; ?>
 
-      <?php if ($page['sidebar_second']): ?>
-        <div id="sidebar-second" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_second']); ?>
-        </div></div> <!-- /.section, /#sidebar-second -->
+      <?php if ($page['content_below']): ?>
+        <div id="content-below" class="column"><div class="section">
+          <?php print render($page['content_below']); ?>
+        </div></div> <!-- /.section, /#content-below -->
       <?php endif; ?>
-
     </div></div> <!-- /#main, /#main-wrapper -->
 
     <div id="footer"><div class="section">
