d7c6d02ff00d3541f90d61b1eaa37394cbb83c0d
Chris Ruppel <chris@fourkitchens.com>
2011-10-20 11:52:07 -0500

    new menu

--- sites/all/themes/webchefcafe/page.tpl.php
+++ sites/all/themes/webchefcafe/page.tpl.php
@@ -97,7 +97,7 @@
 
     <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+        <?php print theme('links__system_main_menu', array('links' => $cafe_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
       </div></div> <!-- /.section, /#navigation -->
     <?php endif; ?>
 
